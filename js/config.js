b39eadc940ca425ce9c755818ef7216314e70429
lumens10 <151405791+lumens10@users.noreply.github.com>
2026-09-23 13:19:29 -0600

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,67 @@
+const invitation = {
+    quinceanera: {
+        name: "Nohemi",
+        age: 15
+    },
+
+    event: {
+        date: "2026-12-19T18:00:00",
+        displayDate: "19 de diciembre de 2026",
+        ceremonyTime: "11:00 AM",
+        receptionTime: "6:00 PM"
+    },
+
+    family: {
+        father: "Luis Angel Rangel Rincon",
+        mother: "Gladiola Flores Antonio",
+        godfather: "Mauricio Aldair Ramírez Luis",
+        godmother: "Cohinda Rodríguez Rojas"
+    },
+
+    ceremony: {
+        name: "Parroquia Santiago Apóstol",
+        maps: "https://goo.gl/maps/bRxn4gBVBFaoiZA67?coh=178571&entry=tt"
+    },
+
+    reception: {
+        name: "Salón Casa Nava",
+        maps: "https://maps.app.goo.gl/1sqdkLtduQHUvZk4A"
+    },
+
+    dressCode: "Formal De Noche",
+
+    music: {
+        title: "Love Story",
+        artist: "Indila",
+        file: "audio/love-story.mp3"
+    },
+
+    rsvp: {
+
+    /* =====================================================
+       NÚMEROS DE WHATSAPP PARA CONFIRMACIÓN
+    ===================================================== */
+
+    option1: {
+        phone: "5579220405"
+    },
+
+    option2: {
+        phone: "5620701794"
+    },
+
+
+    /* =====================================================
+       MENSAJE DE CONFIRMACIÓN
+    ===================================================== */
+
+    message:
+        "Hola, quiero confirmar mi asistencia a los XV años de Nohemi."
+
+},
+
+    pass: {
+        guests: 2
+    }
+};
+
